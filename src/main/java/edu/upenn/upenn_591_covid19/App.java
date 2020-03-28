744c74d993f5db4f18d2dfaad8e9f777a07b5034
Jonathan Li <yrjonathan@gmail.com>
2020-03-27 22:22:07 -0700

    Change message

--- src/main/java/edu/upenn/upenn_591_covid19/App.java
+++ src/main/java/edu/upenn/upenn_591_covid19/App.java
@@ -3,7 +3,7 @@ package edu.upenn.upenn_591_covid19;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 	
 }
